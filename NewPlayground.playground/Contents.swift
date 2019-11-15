82194e1fb51f9774e71baa21aab1d69d3c2c4c7d
JasonHaque <zahinjason220434@gmail.com>
2019-11-15 12:33:55 +0600

    added playground

---
+++ NewPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello from the other side"
+print(str)
+let name = "Sanviraj Zahin Haque"
+print(name)
+var num1 = 5
+var num2 = 10
+print(num1+num2)
+var truth = false
+print(truth)
+
